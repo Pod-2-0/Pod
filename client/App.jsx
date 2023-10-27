66890910d87e1633e3f75258b18d78f50820e175
Binette Maggio <bibaswe@gmail.com>
2023-10-27 07:54:51 -0500

    adding profile page route

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Navigate, BrowserRouter, Routes, Route } from "react-router-dom";
-import "./styles/App.scss";
-import Pages from "./pages/pageLayout/Layout";
-import { AllListings } from "./pages/AllListings/AllListings.jsx";
-import Login from "./pages/SignInUp/Login";
-import SignUp from "./pages/SignInUp/SignUp";
+import React from 'react';
+import { Navigate, BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles/App.scss';
+import Pages from './pages/pageLayout/Layout';
+import { AllListings } from './pages/AllListings/AllListings.jsx';
+import Login from './pages/SignInUp/Login';
+import SignUp from './pages/SignInUp/SignUp';
+import ProfilePage from './pages/ProfilePage.jsx'; // added import for Profile Page
 
-const App  = () => {
-
-    return (
-        <div>
-            <Pages/>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <Pages />
+    </div>
+  );
+};
 
 export default App;
--- client/pages/pageLayout/Layout.js
+++ client/pages/pageLayout/Layout.js
@@ -1,54 +1,54 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route, Link, Switch } from "react-router-dom";
-import styled from "styled-components";
-import Footer from "../../common/Footer"
-import Header from "../../common/Header"
-import Home from "../Home/Home"
-import Login from "../SignInUp/Login";
-import SignUp from "../SignInUp/SignUp";
-import AllListings from "../AllListings/AllListings.jsx";
-import Cart from "../Cart/Cart.jsx";
-
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
+import styled from 'styled-components';
+import Footer from '../../common/Footer';
+import Header from '../../common/Header';
+import Home from '../Home/Home';
+import Login from '../SignInUp/Login';
+import SignUp from '../SignInUp/SignUp';
+import AllListings from '../AllListings/AllListings.jsx';
+import Cart from '../Cart/Cart.jsx';
+import ProfilePage from '../ProfilePage.jsx'; // added import for ProfilePage -BM
 
 const Pages = () => {
-
-    const Page = () => {
-        return (
-            <PageContainer>
-                <Header />
-                <Outlet />
-                <Footer />
-            </PageContainer>
-        )
-    }
-
-
+  const Page = () => {
     return (
-        <>
-        <Router>
-            <Routes>
+      <PageContainer>
+        <Header />
+        <Outlet />
+        <Footer />
+      </PageContainer>
+    );
+  };
 
-                <Route path="/" element={<Page />}>
-                    <Route path ="" element={<Home />} />
-                    <Route path ='/login' element={<Login/>}/>
-                    <Route path ='/signup' element={<SignUp/>}/>
-                    <Route path ='/listing' element={<AllListings/>}/>
-                    <Route path="/cart"  element={<Cart/>}/>
-                    <Route />
-                </Route>
-            </Routes>
-            <Routes>
-                    
-                </Routes>
-        </Router>
-        </>
-    )
-}
-
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Page />}>
+            <Route path='' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/listing' element={<AllListings />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/profile' element={<ProfilePage />} />
+            <Route />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
 const PageContainer = styled.div`
-background-color: #F0F0F0;
-`
+  background-color: #f0f0f0;
+`;
 
 export default Pages;
